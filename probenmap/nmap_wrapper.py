a88e9090f0a33bb0dcd30a62bdca164f2e058936
Ninjeneer <alouache.loan@gmail.com>
2023-05-03 20:38:46 +0200

    fix(nmap): handle single result

--- probenmap/nmap_wrapper.py
+++ probenmap/nmap_wrapper.py
@@ -34,9 +34,16 @@ def scan_server_services(host: str) -> model.ScanResult:
     nb_open_services = len(ports) if ports else 0
     print("[Nmap Scanner] Scan ended - Found {} open services".format(nb_open_services))
 
+    # When there is a single port open, Nmap returns a single object instead of a list
+    # so we need to check the type of the ports
+    portsResult: list = []
+    if isinstance(ports, list):
+        portsResult = list(map(lambda port: Service.build_from_nmap_result(port).__to_dict__(), ports))
+    else:
+        portsResult = [Service.build_from_nmap_result(ports).__to_dict__()]
+
     return model.ScanResult(
-        list(map(lambda port: Service.build_from_nmap_result(
-            port).__to_dict__(), ports)),
+        portsResult,
         model.Context(
             parsed_output['nmaprun']['start'],
             parsed_output['nmaprun']['runstats']['finished']['time']
